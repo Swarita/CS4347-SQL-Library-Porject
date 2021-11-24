eb653e8b6bbd0927eb2682291fd618e6e28773d6
Sampreeth Krovvidi <sampreethkrovvidi@gmail.com>
2021-11-24 05:22:26 -0600

    New_Borrower Update, everything works to add a new borrower into the sql database

--- Library-Management-GUI/src/app/gui/new_borrower.java
+++ Library-Management-GUI/src/app/gui/new_borrower.java
@@ -1,14 +1,19 @@
 package app.gui;
 
 import javax.swing.*;
+import javax.swing.JOptionPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Connection;
 
 public class New_Borrower extends JFrame{
     private JPanel NewBorrowerPanel;
     private JPanel mainPanel;
-    private JLabel LastNameField;
+    private JLabel LastName;
     private JLabel City;
     private JLabel Phone;
     private JLabel Email;
@@ -21,9 +26,9 @@ public class New_Borrower extends JFrame{
     private JTextField SSNField;
     private JTextField AddressField;
     private JTextField StateField;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
+    private JTextField LastNameField;
+    private JTextField CityField;
+    private JTextField PhoneField;
     private JTextField EmailField;
     private JButton AddBorrower;
     private JButton Back;
@@ -45,28 +50,90 @@ public class New_Borrower extends JFrame{
     void createGui() throws SQLException {
         setContentPane(NewBorrowerPanel);
         setSize(800,500);
-        //SearchPanel.setSize(500,500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-
-        String firstName = FirstNameField.getText();
-        String lastName = LastNameField.getText();
-        String ssn = SSNField.getText();
-        String city = City.getText();
-        String address = AddressField.getText();
-        String phone = Phone.getText();
-        String state = StateField.getText();
-        String email = EmailField.getText();
-
-        boolean isValid = true;
-
-
 
         //When the Add a New Borrower Button is pressed
         AddBorrower.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
+                    String firstName = FirstNameField.getText();
+                    String lastName = LastNameField.getText();
+                    String ssn = SSNField.getText();
+                    String city = CityField.getText();
+                    String address = AddressField.getText();
+                    String phone = PhoneField.getText();
+                    String state = StateField.getText();
+                    String email = EmailField.getText();
+
+                    if (firstName.equals("") || lastName.equals("") || ssn.equals("") || city.equals("") || address.equals("") || phone.equals("") || state.equals("") || email.equals("")) {
+                        JOptionPane.showMessageDialog(null, "You must enter all attributes!");
+                    }
+
+                    if (ssn.length() < 9 || ssn.length() > 9) {
+                        JOptionPane.showMessageDialog(null, "The SSN must be 9 digits long!");
+                        SSNField.setText("");
+                    }
+
+                    if (phone.length() > 10 || phone.length() < 10 ) {
+                        JOptionPane.showMessageDialog(null, "The Phone Number length must be 10 digits long!");
+                        PhoneField.setText("");
+                    }
+
+                    //Connection Information
+                    String url = "jdbc:mysql://database-lib.cliese5bfxyl.us-east-2.rds.amazonaws.com:3306/mysql";
+                    String username = "admin";
+                    String password = "lion1234";
+
+                    try {
+                        Connection connection = DriverManager.getConnection(url, username, password);
+                        Statement statement = connection.createStatement();
+
+                        //Checking to see if the following Borrower is in the system, based on the SSN
+                        ResultSet resultSet = statement.executeQuery("SELECT * FROM LIBRARY.BORROWER WHERE Ssn='" + ssn + "';");
+
+
+                        if (resultSet.next()) {
+                            //If the SSN already exists in the Database
+                            JOptionPane.showMessageDialog(null, "The following Borrower already exists ! You must add new Borrowers only !");
+                            SSNField.setText("");
+                        } else {
+                            //If the SSN does not exist in the Database
+                            Statement statementTwo = connection.createStatement();
+
+                            //Checking to see what the max Card ID is
+                            ResultSet resultSetTwo = statementTwo.executeQuery("SELECT max(Card_id) FROM LIBRARY.BORROWER;");
+
+                            int cardId = 0;
+
+                            //Depending on what the max ID is, this will ensure that the new Insert into database has a different Card_ID, which will be 1 up from the max
+                            if (resultSetTwo.next()) {
+                                cardId = resultSetTwo.getInt(1)+1;
+                            }
+
+                            //Concat to full name
+                            String bName = firstName.concat(" " + lastName);
+
+                            //Executing the insert into the Database
+                            Statement statementThree = connection.createStatement();
+                            statementThree.executeUpdate("INSERT INTO `LIBRARY`.`BORROWER` (`Card_id`, `Ssn`, `Bname`, `Address`, `Phone`) VALUES (" + cardId + "," + ssn + ", '" + bName + "', '" + address + "', " + phone + ")");
+
+                            FirstNameField.setText("");
+                            LastNameField.setText("");
+                            SSNField.setText("");
+                            CityField.setText("");
+                            AddressField.setText("");
+                            PhoneField.setText("");
+                            StateField.setText("");
+                            EmailField.setText("");
+
+                            //The following is displaying the Card ID of the new Borrower
+                            JOptionPane.showMessageDialog(null, "The Card Id for the new Borrower is : " + cardId);
+                        }
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
 
                 } catch (Exception e1) {
                     e1.printStackTrace();
